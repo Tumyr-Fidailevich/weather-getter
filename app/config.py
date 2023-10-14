497a05496768fa70d63c9b2290d0f5df3ae329c6
Timur <timurkashapov15@gmail.com>
2023-10-14 12:23:03 +0300

    added new constant BASE_DIR and os import

--- app/config.py
+++ app/config.py
@@ -1,1 +1,7 @@
+from os import getcwd, path
 API_KEY = "123"
+API_CALL = f"https://api.openweathermap.org/data/2.5/weather?lat={'lat'}&lon={'lon'}&appid={API_KEY}&lang=ru"
+BASE_DIR = path.basename(getcwd())
+
+if __name__ == "__main__":
+    print(API_CALL.format())
