431068504730ca942bf2b54e753c6400f1d71540
Timur <timurkashapov15@gmail.com>
2023-10-30 16:25:18 +0300

    added custom exceptions handler in exceptions module

--- app/exceptions.py
+++ app/exceptions.py
@@ -2,6 +2,7 @@ class UserInputException(Exception):
     """
     Basic user input exception.
     """
+
     def __init__(self, message):
         """
         :param message: Message sent to the user.
@@ -13,6 +14,7 @@ class UnknownCityNameError(UserInputException):
     """
     Exception that occurs when the city name is entered incorrectly.
     """
+
     def __init__(self):
         super().__init__("Cannot find this city, try another")
 
@@ -21,6 +23,7 @@ class UnknownCommandError(UserInputException):
     """
     Exception that occurs when an invalid command is entered.
     """
+
     def __init__(self):
         super().__init__("Unsupported command. Type -h for help")
 
@@ -29,5 +32,17 @@ class InvalidBodyRequestError(UserInputException):
     """
     Exception that occurs when an invalid request body is entered.
     """
+
     def __init__(self):
         super().__init__("Invalid request body")
+
+
+def exception_handler(exceptions: tuple = ()):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                func(*args, **kwargs)
+            except exceptions as e:
+                print(e)
+        return wrapper
+    return decorator
