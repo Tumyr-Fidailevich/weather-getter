066cbf5b6a4ba6c3aa74428935c871206d68b1a9
Timur <timurkashapov15@gmail.com>
2023-10-30 22:28:08 +0300

    rework for data_base module. Added the use of the Weather class and removed unnecessary constants. Added get_all_responses function.

--- app/data_base.py
+++ app/data_base.py
@@ -1,83 +1,94 @@
-from typing import List, Dict, Any
 import sqlite3
-from .config import *
-from os import path
+from contextlib import contextmanager
+from .weather_data_type import Weather
 
 
-def insert_response(response: Dict[str, Any]) -> None:
-    """
-    Inserts a preprocessed response into the database.
-    :param response: Preprocessed response for the database.
-    :return: None.
-    :exception: No except.
-    """
-    connection = sqlite3.connect(DB_NAME)
-    cursor = connection.cursor()
-
-    fields = ", ".join(COLUMN_NAMES)
-    values = ', '.join([':' + name for name in COLUMN_NAMES])
-
-    cursor.execute(f'INSERT INTO Weather ({fields}) VALUES ({values})', response)
-
+@contextmanager
+def sqlite_connection(data_base_name: str):
+    connection = sqlite3.connect(data_base_name)
+    yield connection
     connection.commit()
     connection.close()
 
 
-def get_latest_responses(num_of_responses: int, list_all: bool = False) -> List[Dict[str, Any]]:
+def insert_response(connection: sqlite3.Connection, response: Weather) -> None:
+    """
+    Inserts a preprocessed response into the database.
+    :param connection: connection with database.
+    :param response: Preprocessed response for the database.
+    :return: None.
+
+    :exception: No except.
+    """
+    cursor = connection.cursor()
+
+    cursor.execute(
+        f'INSERT INTO Weather ({response.get_fields()}) VALUES ({Weather.get_sqlite_type(response.get_fields())})',
+        dict(response))
+
+
+def get_latest_responses(connection: sqlite3.Connection, num_of_responses: int) -> list[Weather]:
     """
     Returns the latest responses in a prepared form.
+    :param connection: connection with database.
     :param num_of_responses: Number of last requests.
     :param list_all: A key that allows you to display all responses at once.
     :return: Preprocessed response.
     :exception: No except.
     """
-    connection = sqlite3.connect(DB_NAME)
     cursor = connection.cursor()
 
-    if list_all:
-        cursor.execute(
-            "SELECT time, city_name, weather_conditions, "
-            "current_temp, feels_like_temp, wind_speed FROM Weather ORDER BY id DESC")
-    else:
-        cursor.execute(
-            f"SELECT time, city_name, weather_conditions, current_temp, "
-            f"feels_like_temp, wind_speed FROM Weather ORDER BY id DESC LIMIT {num_of_responses}")
-
+    cursor.execute(
+        "SELECT " + "".join(f"{field.name}, " for field in
+                            Weather.get_fields()) + f"FROM Weather ORDER BY id DESC LIMIT {num_of_responses}")
     last_responses = cursor.fetchall()
-    connection.close()
     response_list = list()
 
     for response in last_responses:
-        response_list.append(dict(zip(COLUMN_NAMES, response)))
+        response_list.append(Weather(*response))
 
     return response_list
 
 
-def create_database() -> None:
+def get_all_responses(connection: sqlite3.Connection) -> list[Weather]:
+    """
+    Returns all responses Weather dataclasses.
+    :param connection: connection with database.
+    :return: list of Weather.
+    :exception: No except.
+    """
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT " + "".join(f"{field.name}, " for field in
+                            Weather.get_fields()) + f"FROM Weather ORDER BY id DESC")
+    last_responses = cursor.fetchall()
+    response_list = list()
+
+    for response in last_responses:
+        response_list.append(Weather(*response))
+
+    return response_list
+
+
+def create_database(connection: sqlite3.Connection) -> None:
     """
     Creates a database if it has not already been created.
+    :param connection: connection with database.
     :return: None.
     :exception: No except.
     """
-    connection = sqlite3.connect(DB_NAME)
     cursor = connection.cursor()
-    if not path.exists(DB_NAME):
-        fields = "id INTEGER PRIMARY KEY AUTOINCREMENT, " + ", ".join(
-            f"{column_name} {column_type}" for column_name, column_type in zip(COLUMN_NAMES, COLUMN_TYPES))
-        cursor.execute(f"CREATE TABLE Weather ({fields})")
-        connection.commit()
-    connection.close()
+    fields = "id INTEGER PRIMARY KEY AUTOINCREMENT, " + ", ".join(
+        f"{field.name} {Weather.get_sqlite_type(field.type)}" for field in Weather.get_fields())
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS Weather ({fields})")
 
 
-def drop_database() -> None:
+def drop_database(connection) -> None:
     """
     Drops the database if it exists.
+    :param connection: connection with database.
     :return: None.
     :exception: No except.
     """
-    if path.exists(DB_NAME):
-        connection = sqlite3.connect(DB_NAME)
-        cursor = connection.cursor()
-        cursor.execute('DELETE FROM Weather')
-        connection.commit()
-        connection.close()
+    cursor = connection.cursor()
+    cursor.execute('DELETE FROM Weather IF EXISTS')
